5de853cc77d6ca60b7a75679d5db095757176895
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-04-01 01:01:55 -0500

    Added cart, checkout, index, product-list, product-detail, edit-product, and admin/products views

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,11 @@
 const Product = require('../models/product');
 
-const product = (exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product'
   });
-});
+};
 
 exports.postAddProduct = (req, res, next) => {
   const product = new Product(req.body.title);
@@ -15,10 +15,10 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('shop', {
+    res.render('admin/products', {
       prods: products,
-      pageTitle: 'Shop',
-      path: '/'
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
     });
   });
 };
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 
-const productsController = require('../controllers/products');
+const productsController = require('../controllers/shop');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product', productsController.postAddProduct);
+router.get('/products', adminController.getProducts);
+
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
